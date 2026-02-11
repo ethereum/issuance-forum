b67580cc60162ebc7f9a73ece6548fc5c487031c
Nuno Loureiro <loureironuno@gmail.com>
2026-02-11 10:17:20 +0000

    Update site URL to https://issuance.forum

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://issuance-forum.ethereum.org',
+  site: 'https://issuance.forum',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
